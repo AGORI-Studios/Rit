43c50c6a0d504fcff3f03be4759a944f87535d51
GuglioIsStupid <bradsbluetab@gmail.com>
2022-12-01 18:37:22 -0500

    Add license

--- modules/loveFuncs.lua
+++ modules/loveFuncs.lua
@@ -1,3 +1,24 @@
+--[[----------------------------------------------------------------------------
+
+This file is apart of Rit; a free and open sourced rhythm game made with LÖVE.
+
+Copyright (C) 2022 GuglioIsStupid
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+------------------------------------------------------------------------------]]
+
 if love.system.getOS() ~= "NX" then -- show message box doesn't work on Switch, so use the default error handler
 	-- modified a slight bit from https://github.com/OverHypedDudes/love2dTemplate/blob/main/modules/errHandler.lua
 	function love.errhand(error_message)
