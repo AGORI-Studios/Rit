4361ee4bfd898f50532fbb9b911ccfa6a709104b
Bradley <bradsbluetab@gmail.com>
2023-02-18 13:12:54 -0500

    NX settings file

--- love/settings.lua
+++ love/settings.lua
@@ -21,7 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 local settingsIni = {}
 settings = {}
-settingsStr = [[
+settingsStr = love.system.getOS() ~= "NX" and [[
 [Game]
 ; Makes the notes scroll down instead of up
 ; UPSCROLL IS NOT AS STABLE AS DOWNSCROLL, USE AT YOUR OWN RISK! MAY BREAK VISUALS!
@@ -62,10 +62,44 @@ volume = 1.0
 
 [System]
 version = settingsVer1/0.0.3-beta
-]]
+]] or [[
+; Makes the notes scroll down instead of up
+; UPSCROLL IS NOT AS STABLE AS DOWNSCROLL, USE AT YOUR OWN RISK! MAY BREAK VISUALS!
+downscroll = True
+
+; scroll speed is the scroll speed ever
+scrollspeed = 1.0
+
+; Currently WIP, while it works, it's not recommended to use it
+Scroll Velocities = False
+
+; Start time is for the amount of time to wait before the song starts (In milliseconds)
+startTime = 700
+
+; Note spacing is the amount of space between each note (in pixels) ((its broken a bit tho))
+noteSpacing = 200
+
+; Autoplay - automatically play songs without user input. Can also be toggled with B in song select.
+autoplay = False
+
+; Audio Offset - the amount of time to offset the audio by (in milliseconds)
+audioOffset = 0
+
+[Graphics]
+
+; Vertical sync
+vsync = False
+
+[Audio]
+; Master volume
+volume = 1.0
+
+[System]
+version = settingsVer1/0.0.3-beta
+]] -- NX (Nintendo Switch)
 
 function settingsIni.loadSettings()
-    
+    settings.curSystem = love.system.getOS()
     if not love.filesystem.getInfo("settings.ini") then
         love.filesystem.write("settings.ini", settingsStr)
     end
@@ -82,9 +116,15 @@ function settingsIni.loadSettings()
     settings.autoplay = inifile["Game"]["autoplay"] == "True" or false
     settings.audioOffset = tonumber(inifile["Game"]["audioOffset"]) or 0
 
-    settings.width = tonumber(inifile["Graphics"]["width"]) or 1280
-    settings.height = tonumber(inifile["Graphics"]["height"]) or 720
-    settings.fullscreen = inifile["Graphics"]["fullscreen"] == "True" or false
+    if settings.curSystem ~= "NX" then
+        settings.width = tonumber(inifile["Graphics"]["width"]) or 1280
+        settings.height = tonumber(inifile["Graphics"]["height"]) or 720
+        settings.fullscreen = inifile["Graphics"]["fullscreen"] == "True" or false
+    else
+        settings.width = 1920
+        settings.height = 1080
+        settings.fullscreen = false
+    end
     settings.vsync = inifile["Graphics"]["vsync"] == "True" or false
 
     settings.volume = inifile["Audio"]["volume"] or 1.0
