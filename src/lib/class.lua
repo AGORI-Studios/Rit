e09cdec9bca1e6124953895f1ba4e70f16906b1f
GuglioIsStupid <bradsbluetab@gmail.com>
2023-10-24 12:53:04 -0400

    class comments

--- src/lib/class.lua
+++ src/lib/class.lua
@@ -13,8 +13,15 @@ class.__index = class
 -- Recreate smth like "Table: 0x0000000000000000"
 local idChars = "0123456789abcdef"
 
+--@name class.new
+--@description Creates a new instance of the class
+--@param ... any
+--@return table
 function class:new() end
 
+--@name class.extend
+--@description Creates a new class that extends the current class
+--@return table
 function class:extend()
   local cls = {}
   for k, v in pairs(self) do
@@ -32,29 +39,45 @@ function class:extend()
   return cls
 end
 
+--@name class.implement
+--@description Implements a class into the current class
+--@param ... table
+--@return nil
 function class:implement(...) 
-  for _, cls in pairs({...}) do
-      for k, v in pairs(cls) do
-          if self[k] == nil and type(v) == "function" then
-              self[k] = v
-          end
-      end
-  end
+    for _, cls in pairs({...}) do
+        for k, v in pairs(cls) do
+            if self[k] == nil and type(v) == "function" then
+                self[k] = v
+            end
+        end
+    end
+    return nil
 end
 
+--@name class.isInstanceOf
+--@description Checks if the current class is an instance of the given class
+--@param class table
+--@return boolean
 function class:isInstanceOf(cls)
-  local m = getmetatable(self)
-  while m do
-      if m == cls then return true end
-      m = m.super
-  end
-  return false
+    local m = getmetatable(self)
+    while m do
+        if m == cls then return true end
+        m = m.super
+    end
+    return false
 end
 
+--@name class.__tostring
+--@description Returns the class ID
+--@return string
 function class:__tostring()
   return self.__ID
 end
 
+--@name class.__call
+--@description Creates a new instance of the class
+--@param ... any
+--@return table
 function class:__call(...)
   local inst = setmetatable({}, self)
   inst:new(...)
