26e34e677e7ade49842b205fc889fb7fb54dff9a
GuglioIsStupid <bradsbluetab@gmail.com>
2024-04-19 15:43:31 -0400

    custom error handler

--- src/modules/GameInit.lua
+++ src/modules/GameInit.lua
@@ -53,28 +53,22 @@ function GI.LoadObjects()
 end
 
 function GI.LoadParsers()
-    quaverLoader = require("modules.Parsers.Quaver")
-    osuLoader = require("modules.Parsers.Osu")
-    smLoader = require("modules.Parsers.Stepmania")
-    malodyLoader = require("modules.Parsers.Malody")
-    ritLoader = require("modules.Parsers.Rit")
-    cloneLoader = require("modules.Parsers.Clone")
-
     Parsers = {
-        ["Quaver"] = quaverLoader,
-        ["osu!"] = osuLoader,
-        ["Stepmania"] = smLoader,
-        ["Malody"] = malodyLoader,
-        ["Rit"] = ritLoader,
-        ["CloneHero"] = cloneLoader
+        ["Quaver"] = require("modules.Parsers.Quaver"),
+        ["osu!"] = require("modules.Parsers.Osu"),
+        ["Stepmania"] = require("modules.Parsers.Stepmania"),
+        ["Malody"] = require("modules.Parsers.Malody"),
+        ["Rit"] = require("modules.Parsers.Rit"),
+        ["CloneHero"] = require("modules.Parsers.Clone")
     }
 end
 
 function GI.LoadDefaultFonts()
     Cache.members.font["default"] = love.graphics.newFont("assets/fonts/Montserrat-Light.ttf", 16)
     Cache.members.font["defaultBold"] = love.graphics.newFont("assets/fonts/Montserrat-Medium.ttf", 16)
-    Cache.members.font["menu"] = love.graphics.newFont("assets/fonts/Montserrat-Light.ttf", 32)
-    Cache.members.font["menuBold"] = love.graphics.newFont("assets/fonts/Montserrat-Medium.ttf", 22)
+    Cache.members.font["menu"] = love.graphics.newFont("assets/fonts/Montserrat-Medium.ttf", 32)
+    Cache.members.font["menuBold"] = love.graphics.newFont("assets/fonts/Montserrat-Bold.ttf", 22)
+    Cache.members.font["menuExtraBold"] = love.graphics.newFont("assets/fonts/Montserrat-ExtraBold.ttf", 22)
     Cache.members.font["menuBig"] = love.graphics.newFont("assets/fonts/Montserrat-Light.ttf", 64)
     Cache.members.font["menuBigBold"] = love.graphics.newFont("assets/fonts/Montserrat-Medium.ttf", 64)
     Cache.members.font["menuMedium"] = love.graphics.newFont("assets/fonts/Montserrat-Light.ttf", 48)
@@ -249,6 +243,231 @@ function love.touchmoved(id, x, y, dx, dy, pressure)
     state.touchmoved(id, x, y, dx, dy, pressure)
 end
 
+local utf8 = require("utf8")
+
+local function error_printer(msg, layer)
+	print((debug.traceback("Error: " .. tostring(msg), 1+(layer or 1)):gsub("\n[^\n]+$", "")))
+end
+
+function love.errorhandler(msg)
+    msg = tostring(msg)
+
+	error_printer(msg, 2)
+
+	if not love.window or not love.graphics or not love.event then
+		return
+	end
+
+	if not love.graphics.isCreated() or not love.window.isOpen() then
+		local success, status = pcall(love.window.setMode, 800, 600)
+		if not success or not status then
+			return
+		end
+	end
+
+	-- Reset state.
+	if love.mouse then
+		love.mouse.setVisible(true)
+		love.mouse.setGrabbed(false)
+		love.mouse.setRelativeMode(false)
+		if love.mouse.isCursorSupported() then
+			love.mouse.setCursor()
+		end
+	end
+	if love.joystick then
+		-- Stop all joystick vibrations.
+		for i,v in ipairs(love.joystick.getJoysticks()) do
+			v:setVibration()
+		end
+	end
+	if love.audio then love.audio.stop() end
+
+	love.graphics.reset()
+	local font = love.graphics.setNewFont(14)
+
+	love.graphics.setColor(1, 1, 1)
+
+	local trace = debug.traceback()
+
+	love.graphics.origin()
+
+	local sanitizedmsg = {}
+	for char in msg:gmatch(utf8.charpattern) do
+		table.insert(sanitizedmsg, char)
+	end
+	sanitizedmsg = table.concat(sanitizedmsg)
+
+	local err = {}
+
+	table.insert(err, "Error\n")
+	table.insert(err, sanitizedmsg)
+
+	if #sanitizedmsg ~= #msg then
+		table.insert(err, "Invalid UTF-8 string in error message.")
+	end
+
+	table.insert(err, "\n")
+
+	for l in trace:gmatch("(.-)\n") do
+		if not l:match("boot.lua") then
+			l = l:gsub("stack traceback:", "Traceback\n")
+			table.insert(err, l)
+		end
+	end
+
+	local p = table.concat(err, "\n")
+
+	p = p:gsub("\t", "")
+	p = p:gsub("%[string \"(.-)\"%]", "%1")
+
+    local bg = love.graphics.newImage("assets/images/ui/menu/BGsongList.png")
+
+    local buttons = {
+        {
+            text = "Quit",
+            func = function()
+                love.event.quit()
+            end
+        },
+        {
+            text = "Restart",
+            func = function()
+                love.event.quit("restart")
+            end
+        },
+        {
+            text = "Go to GitHub",
+            func = function()
+                love.system.openURL("https://github.com/AGORI-Studios/Rit/issues")
+            end
+        }
+    }
+
+    local _buttonWidth = 0
+    local _height = 0
+
+	local function draw()
+		if not love.graphics.isActive() then return end
+		local pos = 70
+        -- scale to window
+        love.graphics.push()
+            love.graphics.scale(love.graphics.getWidth()/1280, love.graphics.getHeight()/720)
+            love.graphics.draw(bg, 0, 0, 0, 1280/bg:getWidth(), 720/bg:getHeight())
+            love.graphics.setColor(0, 0, 0, 0.5)
+            
+            -- get length of sanitizedmsg
+            local length = font:getWidth(p)
+            local lines = #(string.split(p, "\n"))
+            local height = (font:getHeight() * lines)*1.85
+            _height = height
+
+            love.graphics.rectangle("fill", pos-15, pos-15, length+35, height+35, 10)
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.printf(p, pos, pos, 1280 - pos)
+
+            love.graphics.setColor(0, 0, 0, 0.5)
+            love.graphics.rectangle("fill", pos + length+50, pos-15, 1280 - pos - length - 100, height+35, 10)
+            love.graphics.setColor(1, 1, 1)
+
+            love.graphics.printf("Press Ctrl+C to copy this error", pos+15 + length+50, pos+5, 1280 - pos - length - 100)
+            love.graphics.printf("Log saved to " .. love.filesystem.getSaveDirectory() .. "/error.log", pos+15 + length+50, pos + 45, 1280 - pos - length - 100)
+
+            -- buttons (tab at the bottom (stretches to the two top tabs)
+            local width = 1280 - pos*2 + 35
+            -- 3 buttons: Quit, Restart, Goto github
+            local buttonWidth = (width-75)/3
+            _buttonWidth = buttonWidth
+            love.graphics.setColor(0, 0, 0, 0.5)
+            love.graphics.rectangle("fill", pos-15, height+pos+35, width, 60, 10)
+            love.graphics.setColor(1, 1, 1)
+
+            love.graphics.push()
+                love.graphics.translate(pos-15, height+pos+40)
+                for i, v in ipairs(buttons) do
+                    love.graphics.setColor(51/255, 10/255, 41/255, 0.75)
+                    local mx, my = love.mouse.getPosition()
+                    local buttonIsHovered = mx > (buttonWidth+15)*(i-1)+75 and mx < (buttonWidth+15)*(i-1)+75+buttonWidth and my > height+pos+35 and my < height+pos+35+50
+                    if buttonIsHovered then
+                        love.graphics.setColor(51/255, 10/255, 41/255, 1)
+                    end
+                    love.graphics.rectangle("fill", (buttonWidth+15)*(i-1)+20, 0, buttonWidth, 50, 10)
+                    --[[ love.graphics.setColor(1, 1, 1)
+                    love.graphics.printf(v.text, (buttonWidth+10)*(i-1)+20, 10+5, buttonWidth, "center") ]]
+                    for x = -1, 1 do
+                        for y = -1, 1 do
+                            love.graphics.setColor(0, 0, 0)
+                            love.graphics.printf(v.text, (buttonWidth+15)*(i-1)+20+x, 10+5+y, buttonWidth, "center")
+                        end
+                    end
+                    love.graphics.setColor(1, 1, 1)
+                    love.graphics.printf(v.text, (buttonWidth+15)*(i-1)+20, 10+5, buttonWidth, "center")
+                    --[[ if buttonIsHovered and love.mouse.isDown(1) then
+                        v.func()
+                    end ]]
+                end
+            love.graphics.pop()
+
+            love.graphics.present()
+        love.graphics.pop()
+	end
+
+	local fullErrorText = p
+    local copied = false
+	local function copyToClipboard()
+		if not love.system then return end
+        if copied then return end -- only copy once, doesn't fill the screen with "Copied to clipboard!"
+        copied = true
+		love.system.setClipboardText(fullErrorText)
+		p = p .. "\nCopied to clipboard!"
+	end
+
+	if love.system then
+		p = p .. "\n\nPress Ctrl+C or tap to copy this error"
+	end
+
+	return function()
+		love.event.pump()
+
+		for e, a, b, c in love.event.poll() do
+			if e == "quit" then
+				return 1
+			elseif e == "keypressed" and a == "escape" then
+				return 1
+			elseif e == "keypressed" and a == "c" and love.keyboard.isDown("lctrl", "rctrl") then
+				copyToClipboard()
+            elseif e == "mousepressed" then
+                local mx, my = love.mouse.getPosition()
+                for i, v in ipairs(buttons) do
+                    local buttonIsHovered = mx > (_buttonWidth+15)*(i-1)+75 and mx < (_buttonWidth+15)*(i-1)+75+_buttonWidth and my > _height+70+35 and my < _height+70+35+50
+                    if buttonIsHovered then
+                        v.func()
+                    end
+                end
+			elseif e == "touchpressed" then
+				local name = love.window.getTitle()
+				if #name == 0 or name == "Untitled" then name = "Game" end
+				local buttons = {"OK", "Cancel"}
+				if love.system then
+					buttons[3] = "Copy to clipboard"
+				end
+				local pressed = love.window.showMessageBox("Quit "..name.."?", "", buttons)
+				if pressed == 1 then
+					return 1
+				elseif pressed == 3 then
+					copyToClipboard()
+				end
+			end
+		end
+
+		draw()
+
+		if love.timer then
+			love.timer.sleep(0.1)
+		end
+	end
+
+end
+test = t()
 -- End of Love Functions
 
 return GI
