32ae5709846f4270b9c2bb642048831db25f88d2
GuglioIsStupid <bradsbluetab@gmail.com>
2024-05-11 19:29:02 -0400

    fixes

--- src/modules/Classes/VertSprite.lua
+++ src/modules/Classes/VertSprite.lua
@@ -1,4 +1,5 @@
-local VertSprite = Sprite:extend()
+local VertSprite = Object:extend()
+VertSprite:implement(Sprite)
 
 local vertexFormat = {
     {"VertexPosition", "float", 2},
@@ -42,16 +43,46 @@ function VertSprite.init()
 end
 
 function VertSprite:new(x, y, z, graphic)
-    self.super.new(self, x, y)
+    self.x, self.y = x or 0, y or 0
+
+    self.graphic = nil
+    self.width, self.height = 0, 0
+
+    self.alive, self.exists, self.visible = true, true, true
+
+    self.origin = Point()
+    self.offset = Point()
+    self.scale = Point(1, 1)
+    self.shear = Point(0, 0)
+
+    self.clipRect = nil
+    self.flipX, self.flipY = false, false
+
+    self.alpha = 1
+    self.color = {1, 1, 1}
+    self.angle = 0 -- ! in degrees
+
+    self.frames = nil -- Todo.
+    self.animations = nil -- Todo.
+
+    self.curAnim = nil
+    self.curFrame = nil
+    self.animFinished = false
+    self.indexFrame = 1
+
+    self.blend = "alpha"
+    self.blendAlphaMode = "alphamultiply"
+
+    self.type = "Image"
+
     self.z = z
     self.offset.z = 0
     self.scale.z = 1
     self.origin.z = 0
     self.fov = 60
-    self.depth = 0
     self.rotation = {x = 0, y = 0, z = 0}
     self.init()
-    self.graphic = love.graphics.newImage(graphic)
+    self.graphic = Cache:loadImage(graphic)
 
     self.vertices = {
 		{0, 0, 0, 0, 0},
@@ -67,20 +98,22 @@ function VertSprite:new(x, y, z, graphic)
 	}
 
     self.mesh = love.graphics.newMesh(vertexFormat, self.vertices, "fan")
+
+    return self
 end
 
 function VertSprite:draw()
     if self.exists and self.alive and self.visible and self.graphic then
         local x = self.x
         local y = self.y
-        local z = self.z
+        local z = self.z or 0
 
         x = x + self.origin.x - self.offset.x
         y = y + self.origin.y - self.offset.y
-        z = z + self.origin.z - self.offset.z
-        local rx, ry, rz = self.rotation.x, self.rotation.y, self.rotation.z
-        local sx, sy, sz = self.scale.x, self.scale.y, self.scale.z
-        local ox, oy, oz = self.origin.x, self.origin.y, self.origin.z
+        z = (z or 0) + (self.origin.z or 0) - (self.offset.z or 0)
+        local rx, ry, rz = self.rotation.x, self.rotation.y, (self.rotation.z or 0)
+        local sx, sy, sz = self.scale.x, self.scale.y, (self.scale.z or 0)
+        local ox, oy, oz = self.origin.x, self.origin.y, (self.origin.z or 0)
 
         local gw, gh = self.graphic:getDimensions()
         local lastColor = {love.graphics.getColor()}
@@ -91,7 +124,7 @@ function VertSprite:draw()
         if not self.flipY then uvy, uvh = uvy + uvh, -uvh end
 
         fw, fh = fw * sx, fh * sy
-        ox,  oy, oz = ox * sx, oy * sy, oz * sz
+        ox, oy, oz = ox * sx, oy * sy, oz * sz
 
         local mesh, verts, length = self.mesh, self.__vertices, #self.vertices
         local vert, vx, vy, vz
@@ -109,7 +142,13 @@ function VertSprite:draw()
         mesh:setVertices(verts)
         love.graphics.setColor(self.color[1], self.color[2], self.color[3], self.alpha)
         love.graphics.setShader(defaultShader)
-        love.graphics.draw(mesh)
+
+        if self.forcedDimensions then
+            local w, h = self:getFrameDimensions()
+            sx = self.dimensions.width / w
+            sy = self.dimensions.height / h
+        end
+        love.graphics.draw(mesh, 0, self.height, 0 --[[ sx, sy ]])
         
         love.graphics.setColor(lastColor)
         love.graphics.setBlendMode(lastBlend, lastAlphaMode)
