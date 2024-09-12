c4f301ea07b527ee6f88dc7024a68a979a2764b8
GuglioIsStupid <bradsbluetab@gmail.com>
2024-09-12 19:30:18 -0400

    Don't load DiscordRPC

--- src/Game/External/init.lua
+++ src/Game/External/init.lua
@@ -1,4 +1,6 @@
 local path = ... .. "."
 
-DiscordRPC = require(path .. "DiscordRPC")
-DiscordRPC:initialize("785717724906913843", true)
+DiscordRPC = nil --require(path .. "DiscordRPC")
+if DiscordRPC then
+    DiscordRPC:initialize("785717724906913843", true)
+end
